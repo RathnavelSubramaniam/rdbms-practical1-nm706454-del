06a56d406c8d3ec1222e0d1fa0c69d051c955b6b
nm706454-del <nm706454@gmail.com>
2026-07-02 13:01:16 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,1 +1,13 @@
 -- Write your SQL statements below
+USE collegeDBBCA;
+CREATE TABLE department (department_ID INT PRIMARY KEY,department_name VARCHAR(100),HOD VARCHAR(100));
+SELECT * FROMN DEPARTMENT;
+
+
+use collegeDBbca;
+alter TABLE student
+add(
+    email VARCHAR(10),
+    phonenumber int(10)
+);
+desc student;
